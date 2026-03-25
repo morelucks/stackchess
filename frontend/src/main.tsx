733ab3eda2cecd9467d93947b5be23d38b50f348
moreluck <luckykamshak@gmail.com>
2026-03-25 08:44:39 +0100

    fix: add stacks connect provider to main entry point

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Connect } from "@stacks/connect-react";
 import App from "./app/app";
 import "./index.css";
 
@@ -8,6 +9,20 @@ if (!rootElement) throw new Error("Root element not found");
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <Connect
+      authOptions={{
+        appDetails: {
+          name: "Stackchess",
+          icon: window.location.origin + "/logo.png",
+        },
+        redirectTo: "/",
+        onFinish: () => {
+          window.location.reload();
+        },
+        userSession: undefined, // Will use default
+      }}
+    >
+      <App />
+    </Connect>
   </StrictMode>
 );
