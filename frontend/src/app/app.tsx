7ec7e25e118c17ccdf2236fe630ae881a69803c7
moreluck <luckykamshak@gmail.com>
2026-03-25 08:46:07 +0100

    fix: add initial session check on app mount

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "../components/pages/LandingPage";
 import ChessScreen from "../components/pages/ChessScreen";
+import useAppStore, { userSession } from "../zustand/store";
 
 function App() {
+  const { setAddress } = useAppStore();
+
+  useEffect(() => {
+    if (userSession.isUserSignedIn()) {
+      const userData = userSession.loadUserData();
+      const address = userData.profile.stxAddress.mainnet || userData.profile.stxAddress.testnet;
+      setAddress(address);
+    }
+  }, [setAddress]);
+
   return (
     <BrowserRouter>
       <Routes>
