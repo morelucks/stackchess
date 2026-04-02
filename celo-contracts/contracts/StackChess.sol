0e2099b44b396afdef302a9b1a4e305260d0056c
morelucks <luckykamshak@gmail.com>
2026-04-02 22:09:53 +0100

    feat(contract): establish internal turn validation and submitMove logic

--- celo-contracts/contracts/StackChess.sol
+++ celo-contracts/contracts/StackChess.sol
@@ -112,3 +112,9 @@ contract StackChess {
         game.boardState = newBoardState;
     }
 
+    function stringEq(string memory a, string memory b) internal pure returns (bool) {
+        return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
+    }
+
+    function resign(uint256 gameId) external {
+        Game storage game = games[gameId];
