02d0bc43a4ad1ea5a64d6d1f3d3379d6afa34527
morelucks <luckykamshak@gmail.com>
2026-04-07 08:14:26 +0100

    test(stacks): test STX-wager creation

--- stacks-contracts/tests/chessxu.test.ts
+++ stacks-contracts/tests/chessxu.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from "vitest";
+import { Cl } from "@stacks/transactions";
 
 const accounts = simnet.getAccounts();
 const deployer = accounts.get("deployer")!;
@@ -7,11 +8,10 @@ const wallet_2 = accounts.get("wallet_2")!;
 const wallet_3 = accounts.get("wallet_3")!;
 
 describe("chessxu - create-game", () => {
-    it("ensures simnet is well initialised", () => {
-        expect(simnet.blockHeight).toBeDefined();
+    it("successfully creates a STX-wagered game", () => {
+        const { result } = simnet.callPublicFn("chessxu", "create-game", [Cl.uint(100), Cl.bool(true)], wallet_1);
+        expect(result).toBeOk(Cl.uint(1));
     });
 });
 
-describe("chessxu - join-game", () => {
-    // Tests will be added here
-});
+
