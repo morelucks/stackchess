5b6640e5e3151b0956d9cc51bb0bbdadb71cca40
morelucks <luckykamshak@gmail.com>
2026-04-11 19:50:57 +0100

    refactor(ui): update PlayerEloCard.tsx to use new useLeaderboard hook

--- frontend/src/components/PlayerEloCard.tsx
+++ frontend/src/components/PlayerEloCard.tsx
@@ -1,4 +1,4 @@
-import { usePlayerStats } from '../chess/hooks/useLeaderboard';
+import { usePlayerStats } from '../hooks/useLeaderboard';
 import StreakBadge from './StreakBadge';
 import './PlayerEloCard.css';
 
